a76ce831a7ed7a273d9f82c08e17941133bd3f6c
Ripal Patel <ripal212007@gmail.com>
2018-08-28 19:09:18 -0500

    added mini challenge

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('hi');
+const cats = ['fluffy', 'spike', 'killer'];
+console.log('1st cat', cats[0]);
+
+const favoriteAnimals = 'cow,bear,dog,cat';
+const favoriteAnimalArray = favoriteAnimals.split(','); //['cow','bear', 'dog', 'cat']
+console.log('favoriteAnimalArray', favoriteAnimalArray);
+
+const numArray = [1, 2, 3, 4, 5];
+const newNum = numArray.join('cow');
+console.log('newNum', newNum);
+
+//mini challenge
+const palChecker = (word) => {
+   
+    const reverseWord = word.split('').reverse().join('');
+    if(word === reverseWord){
+        console.log(`${word} is a palindrome`);
+    } else {
+        console.log(`${word} not a palindrome`);
+    }
+};
+palChecker('mom');
+palChecker('monketbutt');
+palChecker('racecar');
